3eb8036e32a83ca6e5fc2f4ab443933112e8d652
Mauro Servienti <mauro@topics.it>
2015-04-23 14:28:07 +0200

    handlers order sample

---
+++ v5/NSB18HandlersSortOrder/Commands/StartSagaCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSB18HandlersSortOrder.Commands
+{
+	public class StartSagaCommand
+	{
+		public String Sample { get; set; }
+	}
+}
---
+++ v5/NSB18HandlersSortOrder/Program.cs
@@ -0,0 +1,49 @@
+﻿using NServiceBus;
+using NServiceBus.Persistence;
+using Raven.Client.Embedded;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSB18HandlersSortOrder
+{
+	class Program
+	{
+		static void Main( string[] args )
+		{
+			var cfg = new BusConfiguration();
+			cfg.EnableInstallers();
+
+			var embeddedSore = new EmbeddableDocumentStore
+			{
+				ResourceManagerId = new Guid( "{46FFEA87-77A7-43A7-88D3-79778E677D52}" ),
+				DataDirectory = @"~\RavenDB\Data"
+			}.Initialize();
+
+			cfg.UsePersistence<RavenDBPersistence>()
+				.DoNotSetupDatabasePermissions()
+				.SetDefaultDocumentStore( embeddedSore );
+
+			cfg.Conventions()
+				.DefiningCommandsAs( t => t.Namespace != null && t.Namespace.EndsWith( ".Commands" ) )
+				.DefiningEventsAs( t => t.Namespace != null && t.Namespace.EndsWith( ".Events" ) );
+
+			cfg.LoadMessageHandlers( 
+				First<Handlers.SecurityHandler>
+					.Then<Handlers.ValidationHandler>()
+			);
+
+			using( var bus = Bus.Create( cfg ).Start() )
+			{
+				bus.SendLocal( new Commands.StartSagaCommand()
+				{
+					Sample = "Hi, there!"
+				} );
+
+				Console.Read();
+			}
+		}
+	}
+}
