337e8dd24a2370bffae90b5eea667002f23e1576
mauroservienti <mauro@topics.it>
2016-03-16 13:43:51 +0100

    demo mode starter

---
+++ v5/Start saga demo.bat
@@ -0,0 +1,5 @@
+start NSB14Customer\bin\Debug\NSB14Customer.exe -demo
+start NSB14CustomerCare\bin\Debug\NSB14CustomerCare.exe -demo
+start NSB14OrderManager\bin\Debug\NSB14OrderManager.exe -demo
+start NSB14ShippingService\bin\Debug\NSB14ShippingService.exe -demo
+start NSB14WarehouseService\bin\Debug\NSB14WarehouseService.exe -demo
