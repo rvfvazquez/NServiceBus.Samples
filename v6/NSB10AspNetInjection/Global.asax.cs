5ac448fd7bf13e697856de606b255198bebc1196
Mauro Servienti <mauro@topics.it>
2016-03-29 19:14:23 +0200

    10 migrated to V6

--- v6/NSB10AspNetInjection/Global.asax.cs
+++ v6/NSB10AspNetInjection/Global.asax.cs
@@ -9,36 +9,35 @@ using System.Web.SessionState;
 using System.Web.Http;
 using NServiceBus;
 using NServiceBus.ObjectBuilder;
+using Castle.Windsor;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.Lifestyle;
 
 namespace NSB10AspNetInjection
 {
     public class Global : HttpApplication
     {
-        void Application_Start( object sender, EventArgs e )
+        void Application_Start(object sender, EventArgs e)
         {
             // Code that runs on application startup
             AreaRegistration.RegisterAllAreas();
-            GlobalConfiguration.Configure( WebApiConfig.Register );
-            RouteConfig.RegisterRoutes( RouteTable.Routes );
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
 
-            var configuration = new BusConfiguration();
+            var container = new WindsorContainer();
+            container.Register(Component.For<IControllerFactory>().Instance(new BuilderControllerFactory(container)));
+
+            var configuration = new EndpointConfiguration(typeof(Global).Namespace);
+            configuration.UseContainer<WindsorBuilder>(c => c.ExistingContainer(container));
             configuration.UsePersistence<InMemoryPersistence>();
 
-            var busInstance = Bus.Create( configuration ).Start();
-            var builder = ( ( NServiceBus.Unicast.UnicastBus )busInstance ).Builder;
+            var busInstance = Endpoint.Start(configuration).GetAwaiter().GetResult();
 
-            IConfigureComponents configurer = null;
-            configuration.RegisterComponents( r =>
+            DependencyResolver.SetResolver(t =>
             {
-                configurer = r;
-                r.RegisterSingleton<IControllerFactory>( new BuilderControllerFactory( builder ) );
-            } );
-
-            System.Web.Mvc.DependencyResolver.SetResolver( t =>
-            {
-                if( configurer.HasComponent( t ) )
+                if(container.Kernel.HasComponent(t))
                 {
-                    return builder.Build( t );
+                    return container.Resolve(t);
                 }
 
                 //default value expected by MVC to signal that we have no components of type "t"
@@ -46,38 +45,38 @@ namespace NSB10AspNetInjection
             },
             t =>
             {
-                if( configurer.HasComponent( t ) )
+                if(container.Kernel.HasComponent(t))
                 {
-                    return builder.BuildAll( t );
+                    return container.ResolveAll(t).Cast<Object>();
                 }
 
                 //default value expected by MVC to signal that we have no components of type "t"
                 return new List<Object>();
-            } );
+            });
         }
 
         public class BuilderControllerFactory : DefaultControllerFactory
         {
-            readonly IBuilder builder;
+            readonly IWindsorContainer builder;
 
-            public BuilderControllerFactory( IBuilder builder )
+            public BuilderControllerFactory(IWindsorContainer builder)
             {
                 this.builder = builder;
             }
 
-            public override void ReleaseController( IController controller )
+            public override void ReleaseController(IController controller)
             {
-                builder.Release( controller );
+                builder.Release(controller);
             }
 
-            protected override IController GetControllerInstance( RequestContext requestContext, Type controllerType )
+            protected override IController GetControllerInstance(RequestContext requestContext, Type controllerType)
             {
-                if( controllerType == null )
+                if(controllerType == null)
                 {
-                    throw new HttpException( 404, string.Format( "The controller for path '{0}' could not be found.", requestContext.HttpContext.Request.Path ) );
+                    throw new HttpException(404, string.Format("The controller for path '{0}' could not be found.", requestContext.HttpContext.Request.Path));
                 }
 
-                var iController = ( IController )builder.Build( controllerType );
+                var iController = (IController)builder.Resolve(controllerType);
 
                 return iController;
             }
@@ -85,36 +84,34 @@ namespace NSB10AspNetInjection
 
         public class BuilderDependencyResolver : System.Web.Http.Dependencies.IDependencyResolver
         {
-            readonly IBuilder builder;
-            readonly IConfigureComponents configurer;
+            readonly IWindsorContainer builder;
 
-            public BuilderDependencyResolver( IBuilder builder, IConfigureComponents configurer )
+            public BuilderDependencyResolver(IWindsorContainer builder)
             {
                 this.builder = builder;
-                this.configurer = configurer;
             }
 
             public System.Web.Http.Dependencies.IDependencyScope BeginScope()
             {
-                var scope = this.builder.CreateChildBuilder();
-                return new BuilderDependencyScope( scope, this.configurer);
+                var scope = this.builder.BeginScope();
+                return new BuilderDependencyScope(this.builder, scope);
             }
 
-            public object GetService( Type serviceType )
+            public object GetService(Type serviceType)
             {
-                if( this.configurer.HasComponent( serviceType ) )
+                if(this.builder.Kernel.HasComponent(serviceType))
                 {
-                    return this.builder.Build( serviceType );
+                    return this.builder.Resolve(serviceType);
                 }
 
                 return null;
             }
 
-            public IEnumerable<object> GetServices( Type serviceType )
+            public IEnumerable<object> GetServices(Type serviceType)
             {
-                if( this.configurer.HasComponent( serviceType ) )
+                if(this.builder.Kernel.HasComponent(serviceType))
                 {
-                    return this.builder.BuildAll( serviceType );
+                    return this.builder.ResolveAll(serviceType).Cast<Object>();
                 }
 
                 return new Object[ 0 ];
@@ -128,29 +125,29 @@ namespace NSB10AspNetInjection
 
         class BuilderDependencyScope : System.Web.Http.Dependencies.IDependencyScope
         {
-            readonly IBuilder scopedBuilder;
-            readonly IConfigureComponents configurer;
+            readonly IWindsorContainer builder;
+            IDisposable scope;
 
-            public BuilderDependencyScope( IBuilder scopedBuilder, IConfigureComponents configurer )
+            public BuilderDependencyScope(IWindsorContainer scopedBuilder, IDisposable scope)
             {
-                this.scopedBuilder = scopedBuilder;
-                this.configurer = configurer;
+                this.builder = scopedBuilder;
+                this.scope = scope;
             }
-            public object GetService( Type serviceType )
+            public object GetService(Type serviceType)
             {
-                if( this.configurer.HasComponent( serviceType ) )
+                if(this.builder.Kernel.HasComponent(serviceType))
                 {
-                    return this.scopedBuilder.Build( serviceType );
+                    return this.builder.Resolve(serviceType);
                 }
 
                 return null;
             }
 
-            public IEnumerable<object> GetServices( Type serviceType )
+            public IEnumerable<object> GetServices(Type serviceType)
             {
-                if( this.configurer.HasComponent( serviceType ) )
+                if(this.builder.Kernel.HasComponent(serviceType))
                 {
-                    return this.scopedBuilder.BuildAll( serviceType );
+                    return this.builder.ResolveAll(serviceType).Cast<Object>();
                 }
 
                 return new Object[ 0 ];
@@ -158,7 +155,9 @@ namespace NSB10AspNetInjection
 
             public void Dispose()
             {
-                this.scopedBuilder.Dispose();
+                this.scope.Dispose();
+                this.scope = null;
+                GC.SuppressFinalize(this);
             }
         }
     }
