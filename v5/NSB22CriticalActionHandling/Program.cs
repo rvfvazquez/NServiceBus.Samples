62eec0892256397908642702106c78515075f792
mauroservienti <mauro@topics.it>
2016-04-19 13:38:45 +0200

    adding critical errors skeleton

---
+++ v5/NSB22CriticalActionHandling/Program.cs
@@ -0,0 +1,30 @@
+﻿using NServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSB22CriticalActionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cfg = new BusConfiguration();
+
+            cfg.UsePersistence<InMemoryPersistence>();
+            cfg.EnableInstallers();
+
+            cfg.Conventions()
+                .DefiningMessagesAs(t => t.Namespace != null && t.Namespace.EndsWith("Messages"));
+
+            using(var bus = Bus.Create(cfg).Start())
+            {
+
+
+                Console.Read();
+            }
+        }
+    }
+}
